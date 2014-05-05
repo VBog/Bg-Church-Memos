b83ac53d042fee4d62acff66a046d3c1bb84795c
Vadim <vadim.bogaiskov@gmail.com>
2014-05-05 19:28:10 +0400

    Version 1.0

---
+++ js/main.js
@@ -0,0 +1,80 @@
+﻿// Вывести на экран страницу с печатной формой
+function printTab() {
+	saveNames();
+	window.open("paper.php","_self");
+}
+// Загрузка данных в локальное хранилище учетной записи
+function saveNames() {
+	for (var j=0; j<2; j++) {
+		for (var i=0; i<20; i++) {
+			var pos = "o"+j+((i<10)?("0"+i):i);
+			window.localStorage['p_'+pos]=document.getElementById(pos).value;
+		}
+	}
+}
+// Дублировать данные из верхней части формы в нижнюю
+function duplicateDown() {
+	var k;
+	for (var j=0; j<2; j++) {
+		for (var i=0; i<10; i++) {
+			var pos1 = "o"+j+((i<10)?("0"+i):i);
+			window.localStorage['p_'+pos1]=document.getElementById(pos1).value;
+			k=i+10;
+			var pos2 = "o"+j+((k<10)?("0"+k):k);
+			window.localStorage['p_'+pos2]=window.localStorage['p_'+pos1];
+			document.getElementById(pos2).value = window.localStorage['p_'+pos2];
+		}
+	}
+}
+// Дублировать данные из нижней части формы в верхнюю
+function duplicateUp() {
+	var k;
+	for (var j=0; j<2; j++) {
+		for (var i=10; i<20; i++) {
+			var pos1 = "o"+j+((i<10)?("0"+i):i);
+			window.localStorage['p_'+pos1]=document.getElementById(pos1).value;
+			k=i-10;
+			var pos2 = "o"+j+((k<10)?("0"+k):k);
+			window.localStorage['p_'+pos2]=window.localStorage['p_'+pos1];
+			document.getElementById(pos2).value = window.localStorage['p_'+pos2];
+		}
+	}
+}
+// Очистить форму и локальное хранилище учетной записи
+function clearNames() {
+	for (var j=0; j<2; j++) {
+		for (var i=0; i<20; i++) {
+			var pos = "o"+j+((i<10)?("0"+i):i);
+			window.localStorage['p_'+pos] = "";
+			document.getElementById(pos).value = "";
+		}
+	}
+}
+// Загрузка данных из локального хранилища учетной записи
+// в форму ввода
+function main() {
+
+	for (var j=0; j<2; j++) {
+		for (var i=0; i<20; i++) {
+			var pos = "o"+j+((i<10)?("0"+i):i);
+		// Все значения по умолчанию - пусто. 
+			if (localStorage.getItem('p_'+pos) === null){window.localStorage['p_'+pos]="";}
+		// Восстановить сохраненные значения.
+			document.getElementById(pos).value = window.localStorage['p_'+pos];
+		}
+	}
+}
+// в печатную форму
+function load() {
+	for (var j=0; j<2; j++) {
+		for (var i=0; i<20; i++) {
+			var pos = "o"+j+((i<10)?("0"+i):i);
+			if (localStorage.getItem('p_'+pos) === null){window.localStorage['p_'+pos]="";}
+		// Восстановить сохраненные значения.
+			document.getElementById(pos).innerHTML = window.localStorage['p_'+pos];
+		}
+	}
+	window.print();
+	window.close();
+}
+
---
+++ readme.txt
@@ -0,0 +1,62 @@
+﻿=== Bg Church Memos ===
+
+Contributors: Vadim Bogaiskov
+
+Donate link: http://bogaiskov.ru/about-me/donate/
+
+Tags: orthodoxy, christianity, православие, христианство, ορθοδοξία, χριστιανισμός
+
+Requires at least: 3.0.1
+
+Tested up to: 3.9.0
+
+Stable tag: trunk
+
+License: GPLv2
+
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+Orthodox church memos on your site.
+
+
+== Description ==
+
+Plugin allows you create, keep and print church memos for the health and for the repose.
+
+Details in Russian only:
+
+Плагин позволяет создавать, хранить и распечатывать записки в православный храм «О Здравии» и «О Упокоении». 
+
+Чтобы вставить в текст статьи кнопку для вызова календаря используется шорт код  `[memos_button val=' Записки в храм ']`.
+
+Чтобы установить кнопку в виджет, Вы можете воспользоваться следующим html-кодом: `<button onclick='memos_button();'> Записки в храм </button>`
+
+== Installation ==
+
+1. Upload 'bg-church-memos' directory to the '/wp-content/plugins/' directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= На всех ли темах WP работает плагин? =
+
+Для нормальной работы плагина необходимо, чтобы в соответствии с WordPress Codex в файле footer.php Вашей темы WordPress непосредственно перед тегом < /body > вызывалась функция wp_footer();
+
+
+== Screenshots ==
+
+1. Записки в храм
+
+== Changelog ==
+
+= 1.0 =
+
+* Начальная версия. / Start version
+
+== Upgrade Notice ==
+
+== License ==
+
+GNU General Public License v2
+
